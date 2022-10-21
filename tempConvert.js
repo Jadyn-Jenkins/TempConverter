c5223be122bef1ee08b81d0feb62d2da16166766
Jadyn-Jenkins <Noblejadynjenkins@gmail.com>
2022-10-21 11:25:18 -0400

    Finshed JS logic and components. Added simple navbar to three HTMLs. Added ERROR if input isn't a number. Cleaned up HTML.

--- tempConvert.js
+++ tempConvert.js
@@ -2,25 +2,41 @@ let cInput, fInput;
 const convertedField = document.getElementById('convertOut');
 
 const c_fBtn = document.getElementById('cToFBtn');
-if (c_fBtn != null){c_fBtn.addEventListener('click', convertCtoF);}
+if (c_fBtn != null) { c_fBtn.addEventListener('click', convertCtoF); }
 
 const f_cBtn = document.getElementById('fToCBtn');
-if (f_cBtn != null){f_cBtn.addEventListener('click', convertFtoC);}
+if (f_cBtn != null) { f_cBtn.addEventListener('click', convertFtoC); }
+
+const resetBtn = document.getElementById('resetBtn');
+resetBtn.addEventListener('click', resetAll);
 
 window.onload = init();
 
 function init() {
+    // convertCtoF();
+    // convertFtoC();
+    // resetAll();
 }
 
 function convertCtoF() {
-    cInput = Number(document.getElementById('tempInput').value)
-
-    convertedField.innerText = (cInput * (9/5))+32;
+    cInput = Number(document.getElementById('tempInput').value);
+    
+    if (isNaN(cInput)) {
+        alert('ERROR: INPUT MUST BE JUST NUMBERS (1,22,358)');
+    } else {
+        convertedField.innerText = (cInput * (9 / 5)) + 32;
+    }
 }
 
 function convertFtoC() {
     fInput = Number(document.getElementById('tempInput').value)
-
     
-    convertedField.innerText = (fInput-32)*(5/9);
+    if (isNaN(fInput)) {
+        alert('ERROR: INPUT MUST BE JUST NUMBERS (1,22,358)');
+    } else {
+        convertedField.innerText = (fInput - 32) * (5 / 9);
+    }
+}
+function resetAll() {
+    convertedField.innerText = 0;
 }
